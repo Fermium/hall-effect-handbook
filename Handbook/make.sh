f36c1f85252ae631ecfb692dee6c2c02b097bb79
Il <d@fermiumlabs.com>
2016-03-27 17:37:30 +0200

    new export in html

--- Handbook/make.sh
+++ Handbook/make.sh
@@ -3,9 +3,16 @@
 echo "Build started"
 rm  Exports/Hall_Handbook.pdf
 
-echo "Building pdf..."
+echo "Building Pdf..."
 pandoc Hall_Handbook.md --latex-engine=xelatex --filter=pandoc-eqnos -o Exports/Hall_Handbook.pdf
 echo "Pdf built"
+
+echo "Building Html..."
+rm Exports/Hall_Handbook.html
+pandoc Hall_Handbook.md --latex-engine=xelatex --filter=pandoc-eqnos --mathjax -s  -o Exports/Hall_Handbook.html
+cp Media/* Exports/Media/
+echo "Html built"
+
 
 echo "COMPLETE!"
 #open  Exports/Hall_Handbook.pdf
