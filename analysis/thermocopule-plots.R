d40b82903218efe86ccf833818d217b34bd6e87b
Davide Bortolami <d@fermiumlabs.com>
2018-03-21 19:32:24 +0000

    wip

--- analysis/thermocopule-plots.R
+++ analysis/thermocopule-plots.R
@@ -2,36 +2,27 @@ library(here)
 library(tidyverse)
 library(ggplot2)
 library(gridExtra)
+library(readr)
+
 setwd(here())
-nisttable.b<-read.csv(here("analysis/thermocouples", "type_b_out.csv"),sep=",", header=FALSE)
-nisttable.e<-read.csv(here("analysis/thermocouples", "type_e_out.csv"),sep=",", header=FALSE)
-nisttable.j<-read.csv(here("analysis/thermocouples", "type_j_out.csv"),sep=",", header=FALSE)
-nisttable.k<-read.csv(here("analysis/thermocouples", "type_k_out.csv"),sep=",", header=FALSE)
-nisttable.n<-read.csv(here("analysis/thermocouples", "type_n_out.csv"),sep=",", header=FALSE)
-nisttable.r<-read.csv(here("analysis/thermocouples", "type_r_out.csv"),sep=",", header=FALSE)
-nisttable.s<-read.csv(here("analysis/thermocouples", "type_s_out.csv"),sep=",", header=FALSE)
-nisttable.t<-read.csv(here("analysis/thermocouples", "type_t_out.csv"),sep=",", header=FALSE)
-nisttable.b$V2<-nisttable.b$V2*1000000
-nisttable.e$V2<-nisttable.e$V2*1000000
-nisttable.j$V2<-nisttable.j$V2*1000000
-nisttable.k$V2<-nisttable.k$V2*1000000
-nisttable.n$V2<-nisttable.n$V2*1000000
-nisttable.r$V2<-nisttable.r$V2*1000000
-nisttable.s$V2<-nisttable.s$V2*1000000
-nisttable.t$V2<-nisttable.t$V2*1000000
 
+# Import all the tabless
+nisttable.b <- read_csv(here("analysis/thermocouples/type_j_manclean.csv"),col_names = FALSE)
+nisttable.e <- read_csv(here("analysis/thermocouples/type_j_manclean.csv"),col_names = FALSE)
+nisttable.j <- read_csv(here("analysis/thermocouples/type_j_manclean.csv"),col_names = FALSE)
+nisttable.k <- read_csv(here("analysis/thermocouples/type_j_manclean.csv"),col_names = FALSE)
+nisttable.n <- read_csv(here("analysis/thermocouples/type_j_manclean.csv"),col_names = FALSE)
+nisttable.r <- read_csv(here("analysis/thermocouples/type_j_manclean.csv"),col_names = FALSE)
+nisttable.s <- read_csv(here("analysis/thermocouples/type_j_manclean.csv"),col_names = FALSE)
+nisttable.t <- read_csv(here("analysis/thermocouples/type_j_manclean.csv"),col_names = FALSE)
 
-
-
-thermocop.plot<-ggplot()+ 
-  geom_line(data=nisttable.b,aes(x=V1,y=c(0,diff(V2)/diff(V1))),colour='red')+
-  geom_line(data=nisttable.e,aes(x=V1,y=c(0,diff(V2)/diff(V1))),colour='blue')+
-  geom_line(data=nisttable.j,aes(x=V1,y=c(0,diff(V2)/diff(V1))),colour='green')+
-  geom_line(data=nisttable.k,aes(x=V1,y=c(0,diff(V2)/diff(V1))),colour='orange')+
-  geom_line(data=nisttable.n,aes(x=V1,y=c(0,diff(V2)/diff(V1))),colour='magenta')+
-  geom_line(data=nisttable.r,aes(x=V1,y=c(0,diff(V2)/diff(V1))),colour='black')+
-  geom_line(data=nisttable.s,aes(x=V1,y=c(0,diff(V2)/diff(V1))),colour='purple')+
-  geom_line(data=nisttable.t,aes(x=V1,y=c(0,diff(V2)/diff(V1))),colour='sienna3')+ylim(0,100)
-thermocop.plot
-
+# convert from V to uV
+nisttable.b$X2<-nisttable.b$X2*1000000
+nisttable.e$X2<-nisttable.e$X2*1000000
+nisttable.j$X2<-nisttable.j$X2*1000000
+nisttable.k$X2<-nisttable.k$X2*1000000
+nisttable.n$X2<-nisttable.n$X2*1000000
+nisttable.r$X2<-nisttable.r$X2*1000000
+nisttable.s$X2<-nisttable.s$X2*1000000
+nisttable.t$X2<-nisttable.t$X2*1000000
 
