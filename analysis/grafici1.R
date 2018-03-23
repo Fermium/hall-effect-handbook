f24652a6d3b810c7430d32a18528f3b6af3ff1f9
Davide Bortolami <d@fermiumlabs.com>
2018-03-23 17:08:03 +0000

    improved graph appearances

--- analysis/grafici1.R
+++ analysis/grafici1.R
@@ -2,58 +2,6 @@ library(ggplot2)
 library(here)
 library(gridExtra)
 library(ggpubr) #guarda se funziona questa
-#mi sa che questo non va
-# library(showtext)
-# # If you have this font installed 
-# #font.add("gara", "gara.ttf") 
-# # Or you can install a similar one from Google Font 
-# font_add_google("Roboto", "Roboto") 
-# #https://rpubs.com/Koundy/71792 plot carini
-# theme_Publication <- function(base_size=14, base_family="helvetica") {
-#   library(grid)
-#   library(ggthemes)
-#   (theme_foundation(base_size=base_size, base_family=base_family)
-#     + theme(plot.title = element_text(face = "bold",
-#                                       size = rel(1.2), hjust = 0.5),
-#             text = element_text(),
-#             panel.background = element_rect(colour = NA),
-#             plot.background = element_rect(colour = NA),
-#             panel.border = element_rect(colour = NA),
-#             axis.title = element_text(face = "bold",size = rel(1)),
-#             axis.title.y = element_text(angle=90,vjust =2),
-#             axis.title.x = element_text(vjust = -0.2),
-#             axis.text = element_text(), 
-#             axis.line = element_line(colour="black"),
-#             axis.ticks = element_line(),
-#             panel.grid.major = element_line(colour="#f0f0f0"),
-#             panel.grid.minor = element_blank(),
-#             legend.key = element_rect(colour = NA),
-#             legend.position = "bottom",
-#             legend.direction = "horizontal",
-#             legend.key.size= unit(0.2, "cm"),
-#             legend.margin = margin(0, "cm"),
-#             legend.title = element_text(face="italic"),
-#             plot.margin=unit(c(10,5,5,5),"mm"),
-#             strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
-#             strip.text = element_text(face="bold")
-#     ))
-#   
-# }
-# 
-# scale_fill_Publication <- function(...){
-#   library(scales)
-#   discrete_scale("fill","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
-# }
-# 
-# scale_colour_Publication <- function(...){
-#   library(scales)
-#   discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
-# }
-# 
-# 
-# old <- theme_set(theme_Publication(base_family='Roboto'))
-# theme_set(old)
-#experiment data
 
 data = read.csv(here('analysis','data','ltk-hall-ge current', 'experiment_1.csv'),header = T)
 
@@ -85,14 +33,14 @@ grid.arrange(Vhvstime.plot,Rvstime.plot,Tvstime.plot,nrow=3)
 #temperatura vs vh
 tempVSVhall.plot<-ggplot()+
 geom_line(data=data,aes(temp,-Vh),col="blue")+
-xlab(paste("Temperature (",degree,C,")"))+ylab("Vhall (V)")+
+xlab(paste("Temperature ( degree C )" ))+ylab("Vhall (V)")+
 ggtitle("Temperature VS Vhall")+
 theme(plot.title = element_text(hjust = 0.5))
 
 #temperatura vs vr
 tempVSVres.plot<-ggplot()+
 geom_line(data=data,aes(temp,Vr),col="blue")+
-xlab(paste("Temperature (",degree,C,")"))+ylab("Vres (V)")+
+xlab(paste("Temperature ( degree C)" ))+ylab("Vres (V)")+
 ggtitle("Temperature VS Vres")+
 theme(plot.title = element_text(hjust = 0.5))
 
@@ -105,7 +53,7 @@ tempVSVres.smooth<-with(data,smooth.spline(temp,Vr))
 
 tempVSVhall.smooth.plot<-ggplot()+
   geom_line(data=with(tempVSVhall.smooth,data.frame(x,y)),aes(x,y),col="blue")+
-  xlab(paste("Temperature (",degree,C,")"))+ylab("Vhall (V)")+
+  xlab(paste( "Temperature (degree C)" ))+ylab("Vhall (V)")+
   ggtitle("Temperature VS Vhall smooth")+
   theme(plot.title = element_text(hjust = 0.5))
 
@@ -113,7 +61,7 @@ tempVSVhall.smooth.plot
 
 tempVSVres.smooth.plot<-ggplot()+
   geom_line(data=with(tempVSVres.smooth,data.frame(x,y)),aes(x,y),col="blue")+
-  xlab(paste("Temperature (",degree,C,")"))+ylab("Vres (V)")+
+  xlab(paste("Temperature (degree C )" ))+ylab("Vres (V)")+
   ggtitle("Temperature VS Vres smooth")+
   theme(plot.title = element_text(hjust = 0.5))
 
@@ -184,3 +132,4 @@ egvst.plot<-ggplot()+
             ggtitle("Energy Gap VS Temperature")+
             theme(plot.title = element_text(hjust = 0.5))
 egvst.plot
+
